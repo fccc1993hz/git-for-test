b76233047395f9c9f39a1a4e07d9c6e53b68c8fa
xufc <xufc@lizi.com>
2018-12-17 17:53:38 +0800

    first commit

---
+++ src/main/java/com/fccc/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.fccc.git;
+
+/**
+ * Created by fccc on 2018/12/17
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
