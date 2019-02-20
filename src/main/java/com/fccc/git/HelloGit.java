9e064d806709e709066f2bed49e221cea87f7e5f
xufc <xufc@lizi.com>
2019-02-20 14:07:41 +0800

    b push

--- src/main/java/com/fccc/git/HelloGit.java
+++ src/main/java/com/fccc/git/HelloGit.java
@@ -11,5 +11,7 @@ public class HelloGit {
         System.out.println("Merge Dev To Test");
 
         System.out.println("A push something");
+
+        System.out.println("B push something");
     }
 }
