08a13b8b527dcb5bec5c94da0ac8212538a21f6a
xufc <xufc@lizi.com>
2019-02-27 15:46:44 +0800

    del

--- src/main/java/com/fccc/git/Git.java
+++ src/main/java/com/fccc/git/Git.java
@@ -7,7 +7,5 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaa");
         System.out.println("bbbbbbbbbbbbb");
-        System.out.println("ccccccccccccc");
-        System.out.println("ddddddddddddd");
     }
 }
